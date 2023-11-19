518b32818d1f10176b7a8b3d57f656becfca4798
Haniye <esmaeilihaniye@yahoo.com>
2023-11-19 02:27:49 -0800

    -Add generating soldiers

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -1,25 +1,46 @@
+using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
-    [SerializeField] private float speed = 0.2f;
+    [SerializeField] private float _speed = 0.2f;
+    [SerializeField] private Soldier _soldierPrefab;
+    private bool _isTouch;
 
-    void Update()
+    private void Update()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             var touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-            transform.Translate(0, 0, touchDeltaPosition.x * speed);
+            transform.Translate(0, 0, touchDeltaPosition.x * _speed);
         }
 #if UNITY_EDITOR
         if (Input.GetMouseButton(0))
         {
             var mouseDeltaPosition = new Vector3(0, 0, Input.GetAxis("Mouse X"));
-            transform.Translate(mouseDeltaPosition * speed);
+            transform.Translate(mouseDeltaPosition * _speed);
         }
 #endif
+        if (Input.GetMouseButton(0) || Input.touchCount > 0)_isTouch = true;
+        else {_isTouch = false;}
     }
-
+    private void Awake()
+    {
+        GenerateSoldier();
+    }
+    private async void GenerateSoldier() 
+    {
+        while (true) 
+        {
+            if (_isTouch) 
+            {
+                Instantiate(_soldierPrefab, this.transform.position, Quaternion.identity);
+                await Task.Delay(1000);
+            }
+            await Task.Yield();
+        }
+    }
 }
 
 
--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -3,12 +3,25 @@ using UnityEngine.AI;
 
 public class Soldier : MonoBehaviour
 {
-    [SerializeField] private Transform targetPosition;
+    [SerializeField] private Transform _targetTransform;
 
     void Start() 
     {
         NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
-        navMeshAgent.SetDestination(targetPosition.position);
+        navMeshAgent.SetDestination(_targetTransform.position);
+    }
+    private void Update()
+    {
+
+        float distanceToDestination = Vector3.Distance(transform.position, _targetTransform.position);
+        Debug.Log($"{distanceToDestination}");
+
+        if (distanceToDestination < 1.5f) 
+        {
+            Debug.Log("Reach destination");
+            Destroy(this.gameObject);
+        }
+
     }
 
 }
